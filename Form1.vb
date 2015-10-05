8ee3355a2b271dd621a0c71109f54a30c09e67b9
dromval90 <danielromval@gmail.com>
2015-10-04 21:02:06 -0600

    inicializacion de proyecto git vs studio 2012

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        For i = 0 To 3
+            MsgBox("Hi Hello Wordl", MsgBoxStyle.Exclamation, "Titulo Dialog")
+        Next
+
+    End Sub
+
+
+End Class
